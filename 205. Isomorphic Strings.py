889e287f790d8122f0d72bfaee1530ed2cf9b855
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-01-10 16:28:08 +0300

    Update 205. Isomorphic Strings.py

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -1,9 +1,6 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s_index, t_index = [], []
-        for i in s:
-            s_index.append(s.index(i))
-        for j in t:
-            t_index.append(t.index(j))
+        s_index = [s.index(i) for i in s]
+        t_index = [t.index(j) for j in t]
             
         return s_index == t_index
