2f6f9f53ae80e3f32213376797571f33b9aa518f
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-02-08 13:01:31 +0300

    Time: 32 ms (81.23%), Space: 13.9 MB (89.08%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        counter = 1
+        denom = 10 ** 9 + 7
+        if n % 2 == 0:
+            counter *= (pow(5, int(n/2),denom) * pow(4,int(n/2), denom))
+        else:
+            counter *= pow(4,n//2,denom)
+            counter *= pow(5, (n//2) + 1, denom)
+        
+        return counter % denom
