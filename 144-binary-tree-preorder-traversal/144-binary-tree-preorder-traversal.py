14a592e8347e54a7e5ada12e4358bf7849aa565a
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-03-05 22:23:44 +0300

    Time: 50 ms (34.16%), Space: 13.8 MB (79.02%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -5,7 +5,6 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         nodes = []
         def dfs(root):
