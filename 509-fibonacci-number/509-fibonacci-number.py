90d6902c5b954fddec3e40de8998f86425ecf946
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-03-20 04:01:23 +0300

    Time: 41 ms (67.41%), Space: 13.8 MB (97.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -2,7 +2,7 @@ class Solution:
     def fib(self, n: int, memo={}) -> int:
         dp = [0,1]
         
-        for i in range(1,n):
+        for i in range(n):
             dp[-1], dp[-2] = dp[-1] + dp[-2], dp[-1]
 
-        return dp[0] if n == 0 else dp[-1]
+        return dp[0]
