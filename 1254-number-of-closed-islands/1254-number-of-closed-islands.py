0271bc61bbc3a716ef319e41eabf9345bb7e852c
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-03-25 15:45:12 +0300

    Time: 177 ms (56.49%), Space: 15 MB (20.70%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,34 +1,25 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        n , m, visited, total = len(grid), len(grid[0]), set(), 0
+        n, m, islands = len(grid), len(grid[0]), 0
+        is_boundary = lambda i,j: (i == 0 or i == n-1 or j == 0 or j == m-1)
         indices = [(i,j) for i in range(n) for j in range(m)]
         
-        def dfs(row, col, flag):
-            visited.add((row,col))
-            grid[row][col] = flag
-
-            for d in DIR:
-                dx, dy = row + d[0], col + d[1]
-                if (dx,dy) not in visited and inbound(dx,dy) and grid[dx][dy] == 0:
-                    dfs(dx,dy, flag)
-            return 1
-        
-        def boundary(x,y):
-            if x == n - 1 or y == m - 1:
-                return True
-            if x == 0 or y == 0:
-                return True
-        
-        DIR = [[0,1],[1,0],[-1,0],[0,-1]]
-        inbound = lambda x,y: 0<=x<n and 0<=y<m
-        
-        for x,y in indices:
-            if boundary(x,y) and grid[x][y] == 0:
-                dfs(x,y,1)
+        def dfs(i, j):
+            if grid[i][j] == 1: return True
+            
+            valid = not is_boundary(i,j)
+            grid[i][j] = 1
+            
+            up, down, left, right = True,True,True,True
+            if i > 0: up = dfs(i-1,j)
+            if i < n-1: down = dfs(i+1,j)
+            if j > 0: left = dfs(i,j-1)
+            if j < m-1: right = dfs(i,j+1)
                 
-        for a,b in indices:
-            if (a,b) not in visited and grid[a][b] == 0:
-                total += dfs(a,b,0)
-                
-        return total
-                
+            return valid and up and down and left and right
+        
+        for i, j in indices:
+            if grid[i][j] == 0:
+                if dfs(i,j): islands += 1
+                        
+        return islands
