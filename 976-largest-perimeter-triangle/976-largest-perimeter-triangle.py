277bd89ed57d2ce2aae97ef65b5e2b791c30bd3a
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-03-26 15:26:16 +0300

    Time: 377 ms (10.17%), Space: 15.5 MB (50.77%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
@@ -2,12 +2,12 @@ class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         per, nums = 0, sorted(nums, reverse=True)
         
-        def perimeter(n,x,y,z):
-            if n[x]+n[y]>n[z] and n[x]+n[z]>n[y] and n[y]+n[z]>n[x]:
-                return n[x]+n[y]+n[z]
+        def perimeter(x,y,z):
+            if nums[z] + nums[y] > nums[x]: #since sorted, this if is enough
+                return nums[z]+nums[y]+nums[x]
             return 0
         
         for i in range(len(nums)-2):
-            per = max(per, perimeter(nums,i,i+1,i+2))
+            per = max(per, perimeter(i,i+1,i+2))
                     
         return per
