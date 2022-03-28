235d5963a1b35c35bd680c1b6b7d14808878749b
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-03-28 13:52:59 +0300

    Time: 32 ms (95.82%), Space: 13.9 MB (17.39%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,11 +1,10 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        r = 0
-        visited = set()
+        visited, r = set(), 0
+        
         for _ in range(n):
-            print('n', n)
             while n:
-                r, n = r+(n%10)**2, n//10
+                r, n = r+(n%10)**2, n//10  
             if r in visited: return False
             if r == 1: return True
             visited.add(r)
