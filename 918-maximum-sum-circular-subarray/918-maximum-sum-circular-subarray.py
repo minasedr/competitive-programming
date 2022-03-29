28b07e02393f0e9cf79a44dcb39bc140494de3d0
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-03-29 15:25:12 +0300

    Time: 936 ms (18.87%), Space: 19 MB (78.37%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -5,9 +5,10 @@ class Solution:
         min_el = max_el = dp_max = dp_min = nums[0]
 
         for i in range(1, len(nums)):
-            dp_max =  nums[i] + max(dp_max, 0)
-            dp_min =  nums[i] + min(dp_min, 0)
+            dp_max = max(nums[i], dp_max+nums[i])
             max_el = max(max_el, dp_max)
+            
+            dp_min = min(nums[i], dp_min+nums[i])
             min_el = min(min_el, dp_min)
-
-        return max(sum(nums) - min_el, max_el)
+            
+        return max(sum(nums)-min_el, max_el)
