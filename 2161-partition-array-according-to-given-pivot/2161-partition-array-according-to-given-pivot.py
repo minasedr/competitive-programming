fcae0dee66481cfe29529adfe535704917dbc1ad
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-02 20:27:39 +0300

    Time: 2267 ms (31.67%), Space: 31.6 MB (49.96%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,6 +1,10 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        left = [n for n in nums if n < pivot]
-        right = [n for n in nums if n > pivot]
-        middle = [n for n in nums if n == pivot]
+        left, middle, right = [], [], []
+        
+        for n in nums:
+            if n < pivot: left.append(n)
+            elif n > pivot: right.append(n)
+            else: middle.append(n)
+                
         return left + middle + right
