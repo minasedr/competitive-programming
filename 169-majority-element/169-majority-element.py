dcc0f0574bf73dd02b45247e1a251797055fb507
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-02 19:54:40 +0300

    Time: 225 ms (51.99%), Space: 15.5 MB (29.39%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,6 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        store = defaultdict(int)
+        store, n = defaultdict(int), len(nums)
+        
         for i in nums:
             store[i] += 1
-            if store[i] > len(nums)//2: return i
+            if store[i] > n//2: return i
