dfe3b79293c403abb1a28f63f788b0bbed7d1c78
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-04 22:28:23 +0300

    Time: 35 ms (73.20%), Space: 13.9 MB (59.69%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        ans, n = "", len(command)
+        
+        for i in range(n):
+            if command[i] == 'G': ans += 'G'
+            if command[i] == '(':
+                if i < n and command[i+1] == ')':
+                    ans += 'o'
+                else: ans += 'al'
+            
+        return ans
