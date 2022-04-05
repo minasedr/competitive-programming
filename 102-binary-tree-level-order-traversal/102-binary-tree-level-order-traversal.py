9f95d5b7c149aef9fd7129f808b7effb322b0161
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-05 11:55:48 +0300

    Time: 72 ms (9.51%), Space: 14.9 MB (9.56%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -7,13 +7,29 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root: return
-        q, ans = deque([root]), []
-        while q:
-            d = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                d.append(node.val)
-                if node.left: q.append(node.left)
-                if node.right: q.append(node.right)
-            ans.append(d)
+        
+        def height(root):
+            if not root: return 0
+            return max(height(root.left), height(root.right))+1
+        
+        ans = [[] for _ in range(height(root))]
+        
+        def traverse(root, level):
+            if not root: return
+            ans[level].append(root.val)
+            traverse(root.left, level + 1)
+            traverse(root.right, level+1)
+            
+        traverse(root,0)
         return ans
+
+#         q, ans = deque([root]), []
+#         while q:
+#             d = [0] * len(q)
+#             for i in range(len(q)):
+#                 node = q.popleft()
+#                 d[i] = node.val
+#                 if node.left: q.append(node.left)
+#                 if node.right: q.append(node.right)
+#             ans.append(d)
+#         return ans
