8291ecbceda4dc575004df3bda4a9b0a2ba172ea
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-07 14:00:07 +0300

    Time: 76 ms (5.08%), Space: 13.9 MB (18.71%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,12 +1,10 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         pal, n, ans = palindrome, len(palindrome), ""
-        if n == 1: return ""
         
-        for i in range(n):
+        if n == 1: return "" 
+        for i in range(n//2):
             if pal[i] != 'a':
-                ans = pal[:i] + 'a' + pal[i+1:]
-                break
-        isPal = lambda x: x == x[::-1]
-        
-        return pal[:-1] + 'b' if (not ans or isPal(ans)) else ans
+                return pal[:i] + 'a' + pal[i+1:]
+            
+        return pal[:-1] + 'b'
