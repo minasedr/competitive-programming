574832e07068842ea7a4a112d9e142fb7986757e
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-08 23:23:13 +0300

    Time: 131 ms (14.70%), Space: 17.2 MB (10.03%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,14 +1,13 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [[0] * i for i in range(1, len(triangle)+1)]
+        memo, n = {}, len(triangle)
         
-        for i in range(len(triangle)):
-            for j in range(len(triangle[i])):
-                if i == 0: 
-                    dp[i][j] = triangle[i][j]; continue
-                    
-                left = dp[i-1][j-1] if j > 0 else math.inf
-                right = dp[i-1][j] if j < len(triangle[i-1]) else math.inf
-                dp[i][j] = triangle[i][j] + min(left, right)
-                
-        return min(dp[-1])
+        def dfs(i,j):
+            if i == n: return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            
+            memo[(i,j)] = triangle[i][j] + min(dfs(i+1,j) , dfs(i+1,j+1))
+            return memo[(i,j)]
+        
+        return dfs(0,0)
