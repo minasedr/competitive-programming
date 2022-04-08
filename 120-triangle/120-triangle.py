df6995af426defcf74956d05ebcacc81c10f9bd3
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-08 23:27:59 +0300

    Time: 73 ms (83.24%), Space: 15.1 MB (35.35%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,13 +1,25 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        memo, n = {}, len(triangle)
+        dp = [[0] * i for i in range(1, len(triangle)+1)]
         
-        def dfs(i,j):
-            if i == n: return 0
-            if (i,j) in memo:
-                return memo[(i,j)]
+        for i in range(len(triangle)):
+            if i == 0: dp[0] = triangle[0]; continue
+            for j in range(len(triangle[i])):
+                left = dp[i-1][j-1] if j > 0 else math.inf
+                right = dp[i-1][j] if j < len(triangle[i-1]) else math.inf
+                dp[i][j] = triangle[i][j] + min(left, right)
+                
+        return min(dp[-1])
+        
+         
+#         memo, n = {}, len(triangle)
+        
+#         def dfs(i,j):
+#             if i == n: return 0
+#             if (i,j) in memo:
+#                 return memo[(i,j)]
             
-            memo[(i,j)] = triangle[i][j] + min(dfs(i+1,j) , dfs(i+1,j+1))
-            return memo[(i,j)]
+#             memo[(i,j)] = triangle[i][j] + min(dfs(i+1,j) , dfs(i+1,j+1))
+#             return memo[(i,j)]
         
-        return dfs(0,0)
+#         return dfs(0,0)
