52237ff9cd170b159c54d16e3daa836ce0e6f0eb
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-09 22:13:58 +0300

    Time: 32 ms (89.30%), Space: 14.3 MB (7.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,15 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        cache = {}
+        @cache
         def dfs(i,j):
-            if (i,j) in cache:
-                return cache[(i,j)]
             if i >= m or j >= n:
                 return 0
             if (i,j) == (m-1,n-1):
                 return 1
-            down, left = dfs(i+1,j), dfs(i,j+1)
-            cache[(i,j)] = down + left
-            return cache[(i,j)]
+            return dfs(i+1,j) + dfs(i,j+1)
         
         return dfs(0,0)
