d1a3a52f66536e9b783b5318be8fa327ad2f3abf
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-11 11:57:11 +0300

    Time: 27 ms (95.12%), Space: 13.9 MB (20.06%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
@@ -1,9 +1,3 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        if n == 1: return 0
-        parent = self.kthGrammar(n-1, math.ceil(k/2))
-        isKOdd = k%2==1
-        if parent == 1:
-            return 1 if isKOdd else 0
-        else:
-            return 0 if isKOdd else 1
+        return int(bin(k-1).count("1") % 2 == 1)
