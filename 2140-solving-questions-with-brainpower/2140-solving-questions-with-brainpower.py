e2fec26a4a3db66fde21c836398ce51193d317b7
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-15 22:48:08 +0300

    Time: 1695 ms (90.52%), Space: 69.1 MB (45.91%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,14 +1,25 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        n, memo = len(questions), {}
+#         n, memo = len(questions), {}
         
-        def dfs(i):
-            if i >= n: return 0
-            if i in memo: return memo[i]
-            memo[i] = max(
-                dfs(i+questions[i][1]+1) + questions[i][0],
-                dfs(i+1)
+#         def dfs(i):
+#             if i >= n: return 0
+#             if i in memo: return memo[i]
+#             memo[i] = max(
+#                 dfs(i+questions[i][1]+1) + questions[i][0],
+#                 dfs(i+1)
+#             )
+#             return memo[i]
+        
+#         return dfs(0)
+    
+    
+        dp = defaultdict(int)
+        
+        for i in range(len(questions) - 1, -1, -1):
+            dp[i] = max(
+                dp[i + questions[i][1] + 1] + questions[i][0],
+                dp[i + 1]
             )
-            return memo[i]
-        
-        return dfs(0)
+            
+        return dp[0]
