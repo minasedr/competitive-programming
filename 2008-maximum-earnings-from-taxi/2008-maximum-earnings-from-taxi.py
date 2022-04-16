fc7327ddbfd113e275d67c8b7ac344e14301cf64
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-16 15:42:31 +0300

    Time: 1931 ms (85.60%), Space: 57.9 MB (12.25%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -1,11 +1,13 @@
 class Solution:
     def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
-        @cache
-        def dp(i):
+        def dp(i, memo):
             if i >= n: return 0
+            if i in memo: return memo[i]
             l, r, t = rides[i]
             j = bisect_right(rides, [r,0,0])
-            return max(dp(i+1), dp(j)+(r-l+t))
+            memo[i] = max(dp(i+1, memo), dp(j, memo)+(r-l+t))
+            return memo[i]
         
         n, rides = len(rides), sorted(rides)
-        return dp(0)
+        return dp(0, {})
+    
