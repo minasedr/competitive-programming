4843ec0e078e2d92d357878dd2890b33cce96d68
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-19 19:38:37 +0300

    Time: 1041 ms (85.82%), Space: 27.8 MB (16.18%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
@@ -1,8 +1,14 @@
 class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
-        cache = defaultdict(lambda: 1)
         N, pr = len(prices), prices
-        
+        dp = [1] * N
+        for i in range(1, N):
+            if pr[i] - pr[i-1] == -1:
+                dp[i] += dp[i-1]
+                
+        return sum(dp)
+    
+        cache = defaultdict(lambda: 1)
         def dp(i):
             if i >= N: return 1
             if pr[i] - pr[i-1] == -1:
