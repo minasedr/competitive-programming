5b5945d4743f6f4373e53ae6377184c252c6def8
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-19 19:28:34 +0300

    Time: 1850 ms (7.28%), Space: 210 MB (5.27%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.py
@@ -3,9 +3,10 @@ class Solution:
         cache = defaultdict(lambda: 1)
         N, pr = len(prices), prices
         
-        def dfs(i):
+        def dp(i):
             if i >= N: return 1
-            cache[i] = 1 + cache[i-1] if pr[i] - pr[i-1] == -1 else cache[i]
-            return cache[i] + dfs(i+1)
+            if pr[i] - pr[i-1] == -1:
+                cache[i] = 1 + cache[i-1]
+            return cache[i] + dp(i+1)
         
-        return dfs(1)
+        return dp(1)
