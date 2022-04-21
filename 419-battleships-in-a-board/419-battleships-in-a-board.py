9e2a9ecc55f47c2535d51ec71c943b18d960ed9e
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-21 18:24:23 +0300

    Time: 125 ms (25.65%), Space: 14.6 MB (18.72%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -1,21 +1,12 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        visited = set() 
-        
-        def dfs(r, c):
-            visited.add((r,c))
-            for d in DIR:
-                dx, dy = r + d[0], c + d[1]
-                if inbound(dx,dy) and (dx,dy) not in visited and board[dx][dy] == 'X':
-                    dfs(dx,dy)
-            return 1
-        
         N, M, ans = len(board), len(board[0]), 0
-        inbound = lambda x, y: 0 <= x < N and 0 <= y < M
-        DIR = [(1,0), (0,1), (-1,0), (0,-1)]
-        
+
         for i in range(N):
             for j in range(M):
-                if (i,j) not in visited and board[i][j] == 'X':
-                    ans += dfs(i,j)
+                if board[i][j] == 'X':
+                    if (i > 0 and board[i-1][j] == 'X') or (j > 0 and board[i][j-1] == 'X'):
+                        continue
+                    ans += 1
+                       
         return ans
