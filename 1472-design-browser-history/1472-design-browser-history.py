a6379e0d8e0f31b90ccc535549546fec2d9cb0ca
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-21 16:15:50 +0300

    Time: 299 ms (64.96%), Space: 16.8 MB (32.01%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -6,35 +6,34 @@ class Page:
         
 class BrowserHistory:
     def __init__(self, homepage: str):
-        self.first = Page()
-        self.last = Page()
-        self.home = Page(homepage, self.first, self.last)
-        self.first.next = self.home
-        self.last.prev = self.home
+        self.head, self.tail = Page(), Page()
+        self.home = Page(homepage, self.head, self.tail)
+        self.head.next = self.home
+        self.tail.prev = self.home
         self.cur = self.home
         
     def visit(self, url: str) -> None:
-        new = Page(url, self.cur, self.last)
+        new = Page(url, self.cur, self.tail)
         self.cur.next = new
-        self.last.prev = new
+        self.tail.prev = new
         self.cur = self.cur.next
         
     def back(self, steps: int) -> str:
         for _ in range(steps):
             self.cur = self.cur.prev
-            if self.cur == self.first:
-                self.cur = self.first.next
+            if self.cur == self.head:
+                self.cur = self.head.next
                 break
         return self.cur.url
 
     def forward(self, steps: int) -> str:
         for _ in range(steps):
             self.cur = self.cur.next
-            if self.cur == self.last:
-                self.cur = self.last.prev
+            if self.cur == self.tail:
+                self.cur = self.tail.prev
                 break
         return self.cur.url
-
+    
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
 # obj.visit(url)
