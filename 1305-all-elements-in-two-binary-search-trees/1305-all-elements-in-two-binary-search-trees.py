23dae26052d584e5d067df73dfc70802e6484f89
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-22 13:16:23 +0300

    Time: 386 ms (72.16%), Space: 23.1 MB (21.64%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -7,12 +7,25 @@
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
         
-        def dfs(root):
+        def dfs(root, arr):
             if not root: return
-            dfs(root.left)
-            ans.append(root.val)
-            dfs(root.right)
-        ans = []
-        dfs(root1)
-        dfs(root2)
-        return sorted(ans)
+            dfs(root.left, arr)
+            arr.append(root.val)
+            dfs(root.right, arr)
+        
+        arr1, arr2 = [], []
+        
+        dfs(root1, arr1)
+        dfs(root2, arr2)
+        
+        l, r, ans = 0, 0, []
+        N, M = len(arr1), len(arr2)
+        
+        while l < N and r < M:
+            if arr1[l] < arr2[r]:
+                ans.append(arr1[l])
+                l += 1
+            else:
+                ans.append(arr2[r])
+                r += 1
+        return ans + arr1[l:] + arr2[r:]            
