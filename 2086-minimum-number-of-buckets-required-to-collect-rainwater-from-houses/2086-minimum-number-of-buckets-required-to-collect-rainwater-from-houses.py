97ab55aa46e448fca21430833b8cfc0f49720562
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-22 19:08:57 +0300

    Time: 206 ms (33.49%), Space: 15.6 MB (55.05%) - LeetHub

--- 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.py
+++ 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.py
@@ -2,27 +2,24 @@ class Solution:
     def minimumBuckets(self, street: str) -> int:
         N, s = len(street), street
         dp, bd = [0] * N, lambda x: 0 < x < N-1
+        f = lambda x: s[x] == 'H'
         
-        if N == 1 and s[0] == 'H':
+        if N == 1 and f(0):
             return -1
+        
         for i in range(N):
-            if bd(i) and s[i] == 'H':
-                if dp[i-1] == 1:
-                    continue
-                if s[i+1] == '.':
-                    dp[i+1] = 1
-                elif s[i-1] == '.':
-                    dp[i-1] = 1
-                else:
-                    return -1
-            elif i == 0 and s[i] == 'H':
-                if s[i+1] == '.':
-                    dp[i+1] = 1
-                else:
-                    return -1
-            elif i == N-1 and s[i] == 'H':
-                if s[i-1] == '.':
-                    dp[i-1] = 1
-                else:
-                    return -1
+            if bd(i) and f(i):
+                if dp[i-1] == 1: continue
+                if not f(i+1): dp[i+1] = 1
+                elif not f(i-1): dp[i-1] = 1
+                else: return -1
+                
+            elif i == 0 and f(i):
+                if not f(i+1): dp[i+1] = 1
+                else: return -1
+                
+            elif i == N-1 and f(i):
+                if not f(i-1): dp[i-1] = 1
+                else: return -1
+                
         return dp.count(1)
