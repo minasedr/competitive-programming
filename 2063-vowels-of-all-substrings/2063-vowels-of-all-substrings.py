ee6bdf64fa2a63b47d7be68cd18d27563dcc418c
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-22 18:15:31 +0300

    Time: 176 ms (48.91%), Space: 15 MB (45.11%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.py
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.py
@@ -1,12 +1,8 @@
 class Solution:
     def countVowels(self, word: str) -> int:
-        N, vowel = len(word), {'a','e','i','o','u'}
-        dp = [0] * N
-        dp[0] = 1 if word[0] in vowel else 0
-        for i in range(1,N):
-            if word[i] in vowel:
-                dp[i] = dp[i-1] + (i+1)
-            else:
-                dp[i] = dp[i-1]
-                
-        return sum(dp)
+        vowel = {'a','e','i','o','u'}
+        cur = prev = 1 if word[0] in vowel else 0
+        for i in range(1, len(word)):
+            prev = (prev+(i+1) if word[i] in vowel else prev)
+            cur += prev
+        return cur
