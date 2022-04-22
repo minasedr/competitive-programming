f288cfdc7808f30662efa4c70047334dc9648db9
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-04-22 19:31:45 +0300

    Time: 249 ms (23.40%), Space: 15.7 MB (40.83%) - LeetHub

--- 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.py
+++ 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.py
@@ -1,7 +1,8 @@
 class Solution:
     def minimumBuckets(self, street: str) -> int:
         N, s = len(street), street
-        dp, bd = [0] * N, lambda x: 0 < x < N-1
+        res = [0] * N
+        bd = lambda x: 0 < x < N-1
         f = lambda x: s[x] == 'H'
         
         if N == 1 and f(0):
@@ -9,17 +10,17 @@ class Solution:
         
         for i in range(N):
             if bd(i) and f(i):
-                if dp[i-1] == 1: continue
-                if not f(i+1): dp[i+1] = 1
-                elif not f(i-1): dp[i-1] = 1
+                if res[i-1] == 1: continue
+                if not f(i+1): res[i+1] = 1
+                elif not f(i-1): res[i-1] = 1
                 else: return -1
                 
             elif i == 0 and f(i):
-                if not f(i+1): dp[i+1] = 1
+                if not f(i+1): res[i+1] = 1
                 else: return -1
                 
             elif i == N-1 and f(i):
-                if not f(i-1): dp[i-1] = 1
+                if not f(i-1): res[i-1] = 1
                 else: return -1
                 
-        return dp.count(1)
+        return res.count(1)
