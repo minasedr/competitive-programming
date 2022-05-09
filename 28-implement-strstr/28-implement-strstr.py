1d408505a5b2d01a7cab0652de4bc9fa99996dbf
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-05-09 16:41:28 +0300

    Time: 30 ms (91.38%), Space: 13.8 MB (79.05%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,3 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        N, M = len(haystack), len(needle)
+        
+        for i in range(N-M+1):
+            j, k = i, 0
+            while j < N and k < M and haystack[j] == needle[k]:
+                k, j = k+1, j+1
+                
+            if k == M: return i
+        return -1
