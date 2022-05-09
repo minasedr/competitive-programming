6137816273f3dfc711048be2027843f93c41c452
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-05-09 17:52:42 +0300

    Time: 42 ms (51.23%), Space: 13.9 MB (79.68%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,14 +1,12 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res, N = [], len(digits)
-        if N == 0:
-            return ""
-
+        
         chmap = {'2':['a','b','c'], '3':['d','e','f'], 
                 '4':['g','h','i'], '5': ['j','k','l'], 
                 '6':['m','n','o'], '7':['p','q','r','s'], 
                 '8':['t','u','v'], '9':['w','x','y','z']}
-        
+  
         def f(i, cur):
             if len(cur) == N:
                 res.append(cur)
@@ -16,6 +14,6 @@ class Solution:
             
             for ch in chmap[digits[i]]:
                 f(i+1, cur+ch)
-                
             return res
+        
         return f(0, "")
