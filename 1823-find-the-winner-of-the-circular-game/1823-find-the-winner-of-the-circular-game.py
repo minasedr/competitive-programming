fd2376d0ce6f8fb66debe9116c4ae0494c42389e
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-05-13 00:59:46 +0300

    Time: 336 ms (25.11%), Space: 13.8 MB (79.65%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,14 +1,12 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        count = k
         q = deque([i for i in range(1, n+1)])
         if len(q) == 1: return q[0]
+        
         while q:
-            count = k
-            cur = q.popleft()
+            cur, count = q.popleft(), k
             for _ in range(k-1):
                 q.append(cur)
                 cur = q.popleft()
-            if len(q) == 1:
-                return q[0]
+            if len(q) == 1: return q[0]
         
