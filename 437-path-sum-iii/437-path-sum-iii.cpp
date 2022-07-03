395fbf6422de17be1183d0934e8f735825e6e3e3
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-07-03 23:08:17 +0300

    Time: 1803 ms (5.01%), Space: 277.4 MB (5.11%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     long long ans = 0;
     int pathSum(TreeNode* root, long long sum) {
-        map<int, int> pref;
+        unordered_map<int, int> pref;
         pref[0] = 1;
         dfs(root, sum, 0, pref);
         return ans;
     }
     
-    void dfs(TreeNode* root, long long sum, long long cur, map<int,int>pref) {
+    void dfs(TreeNode* root, long long sum, long long cur, unordered_map<int,int>pref) {
         if (!root)
             return;
         cur += root->val;
