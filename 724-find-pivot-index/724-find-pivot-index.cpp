f8661f41724296af86bbc64fbf06b75fac12d7e0
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-07-04 13:41:12 +0300

    Time: 52 ms (20.76%), Space: 31.7 MB (14.98%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>prefix(n+1);
+        
+        for (int i = 1; i <= n ; i++) {
+            prefix[i] = nums[i-1] + prefix[i-1];
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (prefix[i-1] == (prefix[n] - prefix[i]))
+                return i-1;
+        }
+      
+        return -1;
+    }
+};
