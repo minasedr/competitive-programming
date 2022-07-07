800d31afbea77d1d4bbbd95b8d6bb944d3ec5d71
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-07-07 16:13:01 +0300

    Time: 60 ms (21.08%), Space: 13.8 MB (65.79%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -3,5 +3,7 @@ class Solution:
         n = len(nums)
         dp = [0] * n
         for i in range(n):
-            dp[i] = max((dp[i-2] if i-2 >= 0 else 0) + nums[i], (dp[i-1] if i-1>=0 else 0))
+            left = dp[i-2] if i -2 >= 0 else 0
+            right = dp[i-1] if i -1 >= 0 else 0
+            dp[i] = max(left+ nums[i], right)
         return dp[n-1]
