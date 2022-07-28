b6507a9e10407b37cd55117785d23a62640da4d9
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-07-28 23:10:32 +0300

    Time: 59 ms (50.78%), Space: 20.7 MB (39.97%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    string cntsort(string s) {
-        int cnt[26] = {};
-        for (char c : s) {
-            cnt[c - 'a']++;
-        }
-        string t = "";
-        for (int c = 0; c < 26; c++) {
-            t += string(cnt[c], c + 'a');
-        }
-        return t;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> group;
         for (auto x: strs) {
-            group[cntsort(x)].push_back(x);
+            string s = x;
+            sort(s.begin(),s.end());
+            group[s].push_back(x);
         }
         vector<vector<string>>res;
         for (auto p: group)
