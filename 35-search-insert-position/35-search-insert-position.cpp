6e926a18090695ce07dda2e39df09eb1d6ac22ef
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-07-31 19:18:13 +0300

    Time: 0 ms (100.00%), Space: 9.7 MB (22.56%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return lower_bound(nums.begin(),nums.end(), target) - nums.begin();
+        int lo = 0, hi = nums.size();
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] >= target)
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+        return lo;
     }
 };
