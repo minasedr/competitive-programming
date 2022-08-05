c01482b643057b25ef312a9ff4b8b1e8baf42150
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-05 10:51:39 +0300

    Time: 120 ms (75.97%), Space: 52.3 MB (28.19%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        for (auto x: nums) {
+            m[x]++;
+            if (m[x] > 1)
+                return true;
+        }
+        return false;
+    }
+};
