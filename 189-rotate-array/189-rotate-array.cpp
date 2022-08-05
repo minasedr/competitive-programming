b080e58c447667034a18e4c0d80a05e6d6383a61
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-05 16:33:50 +0300

    Time: 61 ms (22.20%), Space: 25 MB (81.85%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,14 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        vector<int>res;
-        if (k) {
-            for (int i = n - k; i < n; i++)
-                res.push_back(nums[i]);
-            for (int i = 0; i < n - k; i++)
-                res.push_back(nums[i]);
-            for (int i = 0; i < n; i++)
-                nums[i] = res[i];
-        }
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin()+k, nums.end());
     };
 };
