c123c088a2ce02a04b2c22763a38fc8e8c080382
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-10 10:41:06 +0300

    Time: 3 ms (97.52%), Space: 7.6 MB (97.55%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>res(n + 1);
+        vector<int>dp(n + 1);
         for (int i = 0; i <= n; i++) {
-            res[i] = __builtin_popcount(i);
+            if (i < 2)
+                dp[i] = i;
+            else
+                dp[i] = dp[i/2] + (i & 1 ? 1 : 0);
         }
-        return res;
+        return dp;
     }
 };
