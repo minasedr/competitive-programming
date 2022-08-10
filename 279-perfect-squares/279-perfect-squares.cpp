66ebbac0469045089badf7c33fc5309ae99d8303
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-10 10:27:37 +0300

    Time: 301 ms (49.14%), Space: 9.4 MB (39.01%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> sq;
-        for (int i = 1; i * i <= n; i++)
-            sq.push_back(i*i);
-        vector<int> dp(n + 1, 1e9);
+        vector<int>dp(n + 1, 1e9), res;
         dp[0] = 0;
-        for (int x = 1; x <= n; x++) {
-            for (auto c: sq) {
-                if (x - c >= 0)
-                    dp[x] = min(dp[x], dp[x-c] + 1);
+        for (int i = 1; i * i <= n; i++) {
+            res.push_back(i*i);
+        }
+        for (int i = 1; i <= n; i++) {
+            for (auto c: res) {
+                if (i - c >= 0)
+                    dp[i] = min(dp[i], dp[i - c] + 1);
             }
         }
         return dp[n];
