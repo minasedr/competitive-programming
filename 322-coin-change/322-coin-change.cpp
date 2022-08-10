a7256deef1d7666db96b0a4c34addd1e4bd8397c
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-10 10:32:34 +0300

    Time: 192 ms (28.59%), Space: 14.4 MB (72.93%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,12 +3,13 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount + 1, 1e9);
         dp[0] = 0;
+        
         for (int x = 1; x <= amount; x++) {
             for (auto c: coins) {
                 if (x - c >= 0)
                     dp[x] = min(dp[x], dp[x-c] + 1);
             }
         }
-        return (dp[amount] == 1e9 ? -1: dp[amount]);
+        return (dp[amount] == 1e9 ? -1 : dp[amount]);
     }
 };
