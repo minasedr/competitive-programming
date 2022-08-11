683dc0bbe759bfd70f82cb6d66bd17cf375ed519
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-11 14:00:09 +0300

    Time: 41 ms (45.08%), Space: 13.7 MB (63.72%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<bool>vis(n, false);
+        int n = isConnected.size(), cnt = n;
+        vector<int>link(n), size(n, 1);
+        iota(link.begin(), link.end(), 0);
         
-        function<void(int)> dfs = [&] (int u) {
-            vis[u] = true;
-            for (int i = 0; i < n; i++) {
-                if (!vis[i] && isConnected[u][i])
-                    dfs(i);
-            }
+        function<int(int)> find = [&](int x) {
+            while (x != link[x])
+                x = link[x];
+            return x;
         };
         
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                dfs(i);
-                ans++;
-            }
-        }
-        return ans;
+        function<void(int, int)> unite = [&](int a, int b) {
+            int roota = find(a), rootb = find(b);
+            if (roota == rootb)
+                return;
+            if (size[a] < size[b])
+                swap(a,b);
+            link[rootb] = roota;
+            size[roota] += size[rootb];
+            cnt--;
+        };
+        
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (isConnected[i][j])
+                    unite(i, j);
+                
+        return cnt;
     }
 };
