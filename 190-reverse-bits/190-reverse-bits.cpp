b38d308eb6354e7b164b4a0d800c56b2e70a7ada
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-11 18:11:28 +0300

    Time: 8 ms (12.62%), Space: 5.8 MB (81.11%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -3,8 +3,7 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
         for (int i = 0; i < 32; i++, n >>= 1) {
-            ans <<= 1;
-            ans |= n & 1;
+            ans = (ans << 1) | n & 1;
         }
         return ans;
     }
