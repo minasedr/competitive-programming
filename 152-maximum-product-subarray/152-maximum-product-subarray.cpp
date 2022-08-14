5b067482f8e7e6f0d3eb267866b1c9c5a85608d7
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-14 11:27:47 +0300

    Time: 7 ms (87.53%), Space: 13.9 MB (26.08%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        vector<pair<int,int>> dp (n);
-        dp[0] = {nums[0], nums[0]};
+        int n = nums.size(), maxProd = 1, minProd = 1, ans = INT_MIN;
         
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < 0) {
-                dp[i].first = min(dp[i-1].second * nums[i], nums[i]);
-                dp[i].second = max(dp[i-1].first * nums[i], nums[i]);
-            } else {
-                dp[i].first = min(dp[i-1].first * nums[i], nums[i]);
-                dp[i].second = max(dp[i-1].second * nums[i], nums[i]);
-            }
-        }
-        int ans = nums[0];
-        for (auto x: dp) {
-            ans = max({ans, x.first, x.second});
+        for (auto n : nums) {
+            if (n < 0)
+                swap(maxProd, minProd);
+            maxProd = max(maxProd * n, n);
+            minProd = min(minProd *n , n);
+            ans = max(ans, maxProd);
         }
         return ans;
     }
