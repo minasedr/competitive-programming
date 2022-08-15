6c126d1a1a592d9a3c19b6535e4507163589bea5
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-15 15:49:25 +0300

    Time: 27 ms (35.81%), Space: 13.4 MB (18.37%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        set<int> row, col;
+        unordered_set<int> row, col;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
