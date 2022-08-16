95cbb6b599e4e435062d69687e71dc0c768da36a
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-16 11:30:06 +0300

    Time: 190 ms (11.02%), Space: 33.9 MB (95.23%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,54 @@
+class Trie {
+    struct TrieNode {
+        bool isEnd;
+        map<char, TrieNode*> children;
+    };
+    
+    TrieNode* root;
+    public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* cur = root;
+        for (auto ch: word) {
+            if (cur->children.count(ch))
+                cur = cur->children[ch];
+            else {
+                TrieNode* newNode = new TrieNode();
+                cur->children[ch] = newNode;
+                cur = newNode;
+            }
+        }
+        cur->isEnd = true;
+    }
+
+    bool search(string word) {
+       TrieNode* cur = root;
+        for (auto ch: word) {
+            if (!cur->children.count(ch))
+                return false;
+            cur = cur->children[ch];
+        }
+        return cur->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+       TrieNode* cur = root;
+        for (auto ch: prefix) {
+            if (!cur->children.count(ch))
+                return false;
+            cur = cur->children[ch];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
