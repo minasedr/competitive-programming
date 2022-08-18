f9804992c9b0e4b299db52f24125ef910af64183
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-18 16:09:33 +0300

    Time: 3 ms (91.61%), Space: 11.1 MB (91.74%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    map<pair<int, bool>, int> memo;
-    int dp(int idx, bool buy, vector<int>& prices) {
-        int n = prices.size();
-        if (idx >= n) return 0;
-        if (memo[{idx, buy}]) return memo[{idx, buy}];
-        int cool = dp(idx + 1, buy, prices);
-        if (buy) {
-            int buying = dp(idx + 1, not buy, prices) - prices[idx];
-            memo[{idx, buy}] = max(buying, cool);
-        } else {
-            int sell = dp(idx + 2, true, prices) + prices[idx];
-            memo[{idx, buy}] = max(sell, cool);
-        }
-        return memo[{idx, buy}];
-    }
     int maxProfit(vector<int>& prices) {
-        return dp(0, true, prices);
+        int n = prices.size();
+        vector<int> hasStock(n), noStock(n), justSold(n);
+        hasStock[0] = -prices[0];
+        justSold[0] = INT_MIN;
+        for (int i = 1; i < n; i++) {
+            noStock[i] = max(noStock[i - 1], justSold[i - 1]);
+            hasStock[i] = max(noStock[i-1] - prices[i], hasStock[i - 1]);
+            justSold[i] = hasStock[i-1] + prices[i];
+        }
+        return max(noStock.back(), justSold.back());
     }
 };
