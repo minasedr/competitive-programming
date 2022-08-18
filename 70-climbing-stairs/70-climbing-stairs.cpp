9020fd7f1e1f9953b2d7570b5029af19a0172857
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-18 09:41:44 +0300

    Time: 0 ms (100.00%), Space: 6.3 MB (15.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    unordered_map<int, int>memo;
+    unordered_map<int, int> memo;
     int dp(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        if (memo[n]) return memo[n];
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        if (memo[n])
+            return memo[n];
         memo[n] = dp(n - 1) + dp(n - 2);
         return memo[n];
     }
