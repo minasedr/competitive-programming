6269f14fc48f75c401e015fe100dc85d11e305e5
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-19 11:30:41 +0300

    Time: 75 ms (52.97%), Space: 19 MB (32.79%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        long long n = piles.size();
-        auto f = [&](int k) {
+        function<bool(int)> f= [&] (long long x) {
             long long cnt = 0;
-            for (int i = 0; i < n; i++) {
-                cnt += (piles[i] - 1) / k + 1;
-            }
+            for (int i = 0; i < piles.size(); i++)
+                cnt += ceil((double)piles[i] / x);
             return cnt <= h;
         };
+        
         long long lo = 1, hi = *max_element(piles.begin(), piles.end());
         while (lo < hi) {
             long long mid = lo + (hi - lo) / 2;
