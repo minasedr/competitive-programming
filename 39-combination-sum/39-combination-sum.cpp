a220338f2bfefbe9a7666e833b9d5f2dc974d7f9
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-19 20:25:57 +0300

    Time: 532 ms (5.01%), Space: 150.5 MB (5.10%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    
-    vector<vector<int>> bt(int i, int cur, vector<int> & tmp, int N, vector<int>& cand, int target) {
-     if (i >= N || cur > target)
-            return {};
-        if (cur == target) {
-            res.push_back(tmp);
+    vector<vector<int>> dp(int idx, int cur, int tar, vector<int> app, vector<int>& cand) {
+        if (idx >= cand.size() || cur >= tar) {
+            if (cur == tar)
+                res.push_back(app);
             return {};
         }
-        for (int j = i; j < N; j++) {
-            tmp.push_back(cand[j]);
-            bt(j, cur + cand[j], tmp, N, cand, target);
-            tmp.pop_back();
+        for (int i = idx; i < cand.size(); i++) {
+            app.push_back(cand[i]);
+            dp(i, cur + cand[i], tar, app, cand);
+            app.pop_back();
         }
         return res;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n = candidates.size();        
-        vector<int> tmp;
-        return bt(0, 0, tmp, n, candidates, target);        
+        vector<int>cur;
+        return dp(0, 0, target, cur, candidates);
     }
 };
