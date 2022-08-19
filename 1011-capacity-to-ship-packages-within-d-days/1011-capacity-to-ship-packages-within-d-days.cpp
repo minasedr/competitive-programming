960fe72e056e2b1be2f3ddd86f7ade363bad56e9
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-19 11:32:51 +0300

    Time: 102 ms (37.88%), Space: 26 MB (67.30%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        auto f = [&](int x) {
+        function<bool(int)> f = [&] (long long x) {
             int cur = 0, cnt = 0;
-            for (int i = 0; i < (int)weights.size(); i++) {
+            for (int i = 0; i < weights.size(); i++) {
                 if (weights[i] > x)
                     return false;
                 if (cur + weights[i] > x) {
@@ -16,13 +16,9 @@ public:
             return cnt <= days;
         };
         
-        int lo = INT_MAX, hi = 0;
-        for (auto x: weights) {
-            lo = min(lo, x);
-            hi += x;
-        }
+       long long lo = 0, hi = accumulate(weights.begin(), weights.end(), 0ll);
         while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
+            long long mid = lo + (hi - lo) / 2;
             if (f(mid))
                 hi = mid;
             else
