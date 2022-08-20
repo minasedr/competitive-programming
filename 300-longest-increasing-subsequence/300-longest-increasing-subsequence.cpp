bdb58570aca75ee8864fbfd602db4016d89af26a
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-20 15:13:07 +0300

    Time: 722 ms (17.59%), Space: 10.8 MB (20.91%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    vector<int> memo;
+    int dp(int idx, int prev, vector<int>& nums){
+        if (idx >= nums.size())
+            return 0;
+        if (memo[prev + 1] != -1)
+            return memo[idx];
+        int take = 0, dont = 0;
+        if (prev == -1 || nums[prev] < nums[idx])
+            take = 1 + dp(idx + 1, idx, nums);
+        dont = dp(idx + 1, prev, nums);
+        memo[prev + 1] = max(take, dont);
+        return memo[prev + 1];
+    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>dp;
-        for (int i = 0; i < (int)nums.size(); i++) {
-            auto it = lower_bound(dp.begin(), dp.end(), nums[i]);
-            if (it == dp.end())
-                dp.push_back(nums[i]);
-            else
-                *it = nums[i];
-        }
-        return dp.size();
+        memo.resize(nums.size() + 1, -1);
+        return dp(0, -1, nums);
     }
 };
+  
