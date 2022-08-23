232fe870793c40d628102452d2f93e23f1f42d3a
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-23 17:01:38 +0300

    Time: 556 ms (20.08%), Space: 12.2 MB (58.64%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int minimumMountainRemovals(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(), ans = 0;
         vector<int> dpf(n, 1), dpb(n, 1);
         
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < i; j++)
                 if (nums[i] > nums[j])
                     dpf[i] = max(dpf[i], dpf[j] + 1);
-            }
-        }
+        
         reverse(nums.begin(), nums.end());
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < i; j++)
                 if (nums[i] > nums[j])
                     dpb[i] = max(dpb[i], dpb[j] + 1);
-            }
-        }
+            
         reverse(dpb.begin(), dpb.end());
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             if (dpf[i] > 1 && dpb[i] > 1)
                 ans = max(ans, dpf[i] + dpb[i] - 1);
-        }
-        cout << ans;
+        
         return n - ans;
     }
 };
