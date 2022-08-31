0f8a6090c1d9038917ad19f011781c85cfbaac5a
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-08-31 13:19:04 +0300

    Time: 9 ms (20.39%), Space: 7.2 MB (6.52%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool check(string str1, string str2) {
+        map<char, int> vis;
+        for(auto str: str1)
+            vis[str]++;
+        
+        for (auto str: str2) {
+            if (vis[str] == 0)
+                return false;
+            vis[str]--;
+        }
+        for (auto [k ,v] : vis)
+            if (v > 0) return false;
+        return true;
+    }
+    bool reorderedPowerOf2(int n) {
+        int cur = 1;
+        while (cur <= 1e9) {
+            if (check(to_string(cur), to_string(n))) // checks if n is permutation of n
+                return true;
+            cur *= 2;
+        }
+        return false;
+    }
+};
