0976be0502de84a8b44dbb4fa809c7282330cf38
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-21 01:05:31 +0300

    Time: 23 ms (15.70%), Space: 9.5 MB (87.96%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for (auto x: nums)
-            mp[x]++;
-        for (auto [k,v]: mp)
-            if (v == 1)
-                return k;
-        return 0;
+        int ans = 0, n = nums.size();
+        
+        for (int i = 0; i < 32; i++) {
+            int bit = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] & (1 << i))
+                    bit++;
+            }
+            if (bit % 3)
+                ans |= (1 << i);
+        }
+        return ans;
     }
 };
