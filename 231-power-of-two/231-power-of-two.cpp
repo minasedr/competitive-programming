c485b92f7372cec0edc93c45409b303410ba95af
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-20 23:48:06 +0300

    Time: 3 ms (60.94%), Space: 5.8 MB (89.04%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n < 0) return false;
-        for (int i = 31; i >= 0; i--) {
-            if (n == (1 << i))
-                return true;
-        }
-        return false;
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
     }
 };
