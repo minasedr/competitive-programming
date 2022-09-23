8bf82a0651e7c6eaaa5a5418ee274a277c8941e2
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-23 22:34:01 +0300

    Time: 386 ms (39.47%), Space: 19.2 MB (89.97%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         int n = scores.size(), ans = 0;
-        vector<vector<int>> v;
-        vector<int> dp(n);
+        vector<int>dp(n);
+        vector<pair<int,int>>v(n);
         
-        for (int i = 0; i < n; i++)
-            v.push_back({ages[i], scores[i]});
+        for (int i = 0; i < n; i++) {
+            v[i].first = ages[i];
+            v[i].second = scores[i];
+        }
         sort(v.begin(), v.end());
         
         for (int i = 0; i < n; i++) {
-            dp[i] = v[i][1];
+            dp[i] = v[i].second;
             for (int j = 0; j < i; j++) {
-                if (v[i][1] >= v[j][1])
-                    dp[i] = max(dp[i], dp[j] + v[i][1]);
+                if (v[j].second <= v[i].second)
+                    dp[i] = max(dp[i], dp[j] + v[i].second);
             }
             ans = max(ans, dp[i]);
         }
