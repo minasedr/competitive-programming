f48e409c315bbf9071b873a23ad1edb554ade781
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-25 00:21:04 +0300

    Time: 9 ms (31.67%), Space: 7.3 MB (40.44%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:  
     int wiggleMaxLength(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>up(n), down(n);
+        int n = nums.size(), up = 0, down = 0;
         
         if (n < 2) return n;
-        
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j])
-                    up[i] = max(up[i], down[j] + 1);
-                else if (nums[i] < nums[j])
-                    down[i] = max(down[i], up[j] + 1);
-            }
+            if (nums[i] > nums[i - 1]) 
+                up = down + 1;
+            else if (nums[i] < nums[i - 1])
+                down = up + 1;
         }
-        return max(up.back(), down.back()) + 1;
+        return max(up, down) + 1;
     }
 };
