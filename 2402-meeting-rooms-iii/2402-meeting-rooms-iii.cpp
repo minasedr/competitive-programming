5b09821a291089053ed7a2510b7716dde56e9b06
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-27 00:33:38 +0300

    Time: 1246 ms (23.04%), Space: 129.2 MB (12.19%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -4,19 +4,28 @@ public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
         vector<int> cnt(n);
         sort(meetings.begin(), meetings.end());
-        multiset<pair<i64, int>> pq;
+        multiset<pair<i64, i64>> onuse;
+        multiset<i64> free;
         
-        for (int i = 0; i < n; ++i)
-            pq.insert({meetings[0][0], i});
+        for (int i = 0; i < n; i++)
+            free.insert(i);
+        
         for (auto &m : meetings) {
-            while (pq.begin()->first < m[0]) {
-                pq.insert({m[0], pq.begin()->second});
-                pq.erase(pq.begin());
+            while (!onuse.empty() && onuse.begin()->first <= m[0]) {
+                free.insert(onuse.begin()->second);
+                onuse.erase(onuse.begin());
             }
-            auto [start, room] = *pq.begin();
-            pq.erase(pq.begin());
-            pq.insert({start + m[1] - m[0], room});
+            
+            i64 dur = m[1] - m[0];
+            i64 start = free.empty() ? onuse.begin()->first : m[0];
+            i64 room = free.empty() ? onuse.begin()->second : *free.begin();
+            if (free.empty())
+                onuse.erase(onuse.begin());
+            else
+                free.erase(free.begin());
             cnt[room]++;
+            onuse.insert({start + dur, room});
+
         }
         return max_element(cnt.begin(), cnt.end()) - cnt.begin();
     }
