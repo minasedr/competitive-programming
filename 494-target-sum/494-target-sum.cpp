ac9cfc13c616482058da70b34a71052d031dfa36
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-26 12:27:32 +0300

    Time: 67 ms (46.28%), Space: 22.4 MB (26.24%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -8,13 +8,13 @@ public:
         dp[0][-nums[0] + total] += 1;
         
         for (int i = 1; i < n; i++) {
-            for (int sum = -total; sum <= total; sum++) {
-                if (dp[i-1][sum + total] > 0) {
-                    dp[i][sum + nums[i] + total] += dp[i-1][sum + total];
-                    dp[i][sum - nums[i] + total] += dp[i-1][sum + total];
+            for (int j = -total; j <= total; j++) {
+                if (dp[i - 1][j + total] > 0) {
+                    dp[i][j + nums[i] + total] += dp[i - 1][j + total];
+                    dp[i][j - nums[i] + total] += dp[i - 1][j + total];
                 }
             }
         }
-        return (abs(target) > total ? 0: dp[n - 1][target + total]);
+        return abs(target) > total ? 0 : dp[n - 1][target + total];
     }
 };
