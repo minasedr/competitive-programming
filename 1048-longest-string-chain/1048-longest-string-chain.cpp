2b33dedeb8d5b5f916b55fa4fce2f9f0ec7e294a
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-27 16:00:34 +0300

    Time: 588 ms (9.91%), Space: 27.1 MB (22.37%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        int n = words.size();
-        sort(words.begin(), words.end(), [](string s, string t) {
-            return s.size() < t.size();
-        });
+        int n = words.size(), mx = 0;
         map<string, int> dp;
-        int mx = 1;
+        sort(words.begin(), words.end(), [](string a, string b) {
+            return a.size() < b.size();
+        });
+        
         for (auto word: words) {
-            dp[word] = 1;
             for (int j = 0; j < word.size(); j++) {
-                string s = word.substr(0, j) + word.substr(j + 1);
-                if (dp.count(s)) {
-                    dp[word] = max(dp[word], dp[s] + 1);
-                    mx = max(mx ,dp[word]);
-                }
+                string w = word.substr(0, j) + word.substr(j + 1);
+                if (dp.count(w))
+                    dp[word] = max(dp[word], dp[w] + 1);
+                mx = max(mx, dp[word]);
             }
         }
-        return mx;
+        return mx + 1;
     }
 };
