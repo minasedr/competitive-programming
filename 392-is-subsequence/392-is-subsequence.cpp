e9a1cbfaa8023349f8082b52d161fa87cd6560d3
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-09-27 15:08:03 +0300

    Time: 7 ms (19.04%), Space: 8 MB (8.54%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int idx = 0, n = s.size(), m = t.size();
-        if (m < n) return false;
-        
-        for (int i = 0; i < m; i++) {
-            if (s[idx] == t[i])
-                idx++;
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
-        return idx == n;
+        return dp[n][m] == n;
     }
 };
