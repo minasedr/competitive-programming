0f3e5e256fb62d7069e99897a033def27e2ffaac
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-02 00:01:10 +0300

    Time: 340 ms (29.54%), Space: 78.8 MB (8.46%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        deque<int> mnq, mxq;
-        int ans = 0, start = 0, n = nums.size();
+        multiset<int> ms;
+        int ans = 1, start = 0, n = nums.size();
         
         for (int end = 0; end < n; end++) {
-            while (!mnq.empty() && nums[mnq.back()] >= nums[end])
-                mnq.pop_back();
-            mnq.push_back(end);
+            ms.insert(nums[end]);
+            while (*ms.rbegin() - *ms.begin() > limit)
+                ms.erase(ms.find(nums[start++]));
             
-            while (!mxq.empty() && nums[mxq.back()] <= nums[end])
-                mxq.pop_back();
-            mxq.push_back(end);
-            
-            while (nums[mxq[0]] - nums[mnq[0]] > limit) {
-                if (mxq[0] <= start)
-                    mxq.pop_front();
-                if (mnq[0] <= start)
-                    mnq.pop_front();
-                start++;
-            }
             ans = max(ans, end - start + 1);
         }
         return ans;
