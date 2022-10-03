de003459034f1343a93ab76e2b31e75167af63e0
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-03 09:59:56 +0300

    Time: 166 ms (36.48%), Space: 69.4 MB (87.60%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int ans = 0, remain = 0, cur = 0;
+        
         for (int i = 0; i < gas.size(); i++) {
             cur += gas[i] - cost[i];
             if (cur < 0) {
@@ -10,6 +11,6 @@ public:
                 cur = 0;
             }
         }
-        return cur + remain >= 0 ? ans : -1;
+        return cur + remain >= 0? ans: -1;
     }
 };
