a909ecdcc9a65851a1d00b54ca7f69e8ea1c0313
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-04 17:12:31 +0300

    Time: 7 ms (13.59%), Space: 5.8 MB (96.28%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    double myPow(double x, long long n) {
+    double myPow(double x, long n) {
         if (n == 0) return 1;
-        if (n < 0) return myPow(1 / x, -n);
-        double half =  myPow(x, n / 2);
-        if (n % 2 == 0)
+        if (n < 0)
+            return myPow(1 / x, -n);
+        if (n % 2 == 0) {
+            double half = myPow(x, n / 2);
             return half * half;
-        return half * half * x;
-      
+        }
+        return x * myPow(x, n - 1);   
     }
 };
