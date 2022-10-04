7865373ca0f1e483e97897a1241f77fb480f98ac
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-04 15:37:10 +0300

    Time: 20 ms (92.59%), Space: 21.4 MB (16.99%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -1,22 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    long long ans = 0;
-    int pathSum(TreeNode* root, long long sum) {
-        unordered_map<int, int> pref;
-        pref[0] = 1;
-        dfs(root, sum, 0, pref);
-        return ans;
-    }
+    unordered_map<long, int> cnt{{0, 1}};
+    long ans = 0;
     
-    void dfs(TreeNode* root, long long sum, long long cur, unordered_map<int,int>pref) {
-        if (!root)
+    void dfs(TreeNode* root, long cur, int target) {
+        if (not root)
             return;
         cur += root->val;
-        ans += pref[cur- sum];
-        pref[cur]++;
-        dfs(root->left, sum , cur, pref);
-        dfs(root->right, sum, cur, pref);
-        pref[cur]--;
+        ans += cnt[cur - target];
+        cnt[cur]++;
+        dfs(root->left, cur, target);
+        dfs(root->right, cur, target);
+        cnt[cur]--;
     }
-
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        dfs(root, 0, targetSum);
+        return ans;
+    }
 };
