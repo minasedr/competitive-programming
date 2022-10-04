ea47f0d05d7672a3165c2533ac9a4432ccdf9f7f
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-04 14:23:48 +0300

    Time: 604 ms (8.13%), Space: 115.9 MB (10.84%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        map<int, int>visited{{0,-1}};
-        int cur_sum = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            cur_sum = (cur_sum + nums[i]) % k;
-            if (!visited.count(cur_sum))
-                visited[cur_sum] = i;
-            else if (i - visited[cur_sum] > 1)
-                return true;
+        unordered_map<int, int> cnt{{0, -1}};
+        int ans = 0, cur = 0, n = nums.size();
+        // 2, 0, 3
+        for (int i = 0; i < n; i++) {
+            (cur += nums[i]) %= k;
+            if (cnt.count(cur))
+                ans += (i - cnt[cur] > 1);
+            else
+                cnt[cur] = i;
         }
-        return false;
+        return ans == 0 ? false: true;
     }
 };
