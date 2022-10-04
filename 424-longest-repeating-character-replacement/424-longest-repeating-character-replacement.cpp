8051a3c265e0d4f17eb8a3e0816e59ad58ca325c
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-04 20:04:21 +0300

    Time: 13 ms (73.12%), Space: 7 MB (84.97%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size(), ans = 0;
+        int start = 0, maxCnt = 0;
+        vector<int>cnt(26);
         
-        for (int i = 0; i < 26; i++) {
-            vector<int> pref(n + 1);
-            
-            for (int j = 1; j <= n; j++)
-                pref[j] = pref[j - 1] + (s[j - 1] != char('A' + i));
- 
-            int start = 0;
-            for (int end = 1; end <= n; end++) {
-                while (pref[end] - pref[start] > k)
-                    start++;
-                ans = max(ans, end - start);
+        for (int end = 0; end < n; end++) {
+            maxCnt = max(maxCnt, ++cnt[s[end] - 'A']);
+            while (end - start + 1 - maxCnt > k) {
+                cnt[s[start] - 'A']--;
+                start++;
             }
+            ans = max(ans, end - start + 1);
         }
         return ans;
     }
