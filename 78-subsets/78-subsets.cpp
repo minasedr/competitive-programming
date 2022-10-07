57b16defaf228cebf2d81fdab7ce693e78556d64
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-07 21:12:05 +0300

    Time: 7 ms (37.36%), Space: 6.9 MB (99.27%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,9 +3,10 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
         int n = nums.size();
+        
         for (int i = 0; i < (1 << n); i++) {
             vector<int> cur;
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < 32; j++) {
                 if (i & (1 << j))
                     cur.push_back(nums[j]);
             }
