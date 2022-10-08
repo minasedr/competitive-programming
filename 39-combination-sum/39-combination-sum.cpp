bfe595ad19f337f89c1aedf633342b42d92d4567
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-08 14:17:10 +0300

    Time: 112 ms (21.97%), Space: 24.9 MB (25.85%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     vector<vector<int>> res;
     vector<int> cur;
-    void f(int i, int sum, vector<int> cand, int target) {
-        if (sum >= target || i >= cand.size()) {
-            if (sum == target)
+    set<set<int>> vis;
+    
+    void dfs(int idx, int sum, int target, vector<int>& cand) {
+        if (sum >= target || idx >= cand.size()) {
+            set<int> st(cur.begin(),cur.end());
+            if (sum == target && !vis.count(st))
                 res.push_back(cur);
             return;
         }
-        
-        for (int j = i; j < cand.size(); j++) {
-            cur.push_back(cand[j]);
-            f(j, sum + cand[j], cand, target);
+        for (int i = idx; i < cand.size(); i++) {
+            cur.push_back(cand[i]);
+            dfs(i, sum + cand[i], target, cand);
             cur.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        f(0, 0, candidates, target);
+        dfs(0, 0, target, candidates);
         return res;
     }
 };
