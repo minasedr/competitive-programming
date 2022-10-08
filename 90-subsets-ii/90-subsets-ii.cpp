106c3c54be5eaf6b74a0932c069262125ecb07e5
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-08 14:38:12 +0300

    Time: 23 ms (7.60%), Space: 20.2 MB (5.17%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> res;
-        int n = nums.size();
-        set<vector<int>> vis;
-        
-        for (int i = 0; i < (1 << n); i++) {
-            multiset<int> st;
-            for (int j = 0; j < 32; j++) {
-                if (i & (1 << j))
-                    st.insert(nums[j]);
-            }
-            vector<int> cur(st.begin(), st.end());
-            if (!vis.count(cur))
+    vector<vector<int>> res;
+    set<multiset<int>> vis;
+    vector<int> cur;
+    void dfs(int idx, vector<int> &nums) {
+        if (idx >= nums.size()) {
+            multiset<int> st(cur.begin(), cur.end());
+            if (!vis.count(st))
                 res.push_back(cur);
-            vis.insert(cur);
+            vis.insert(st);
+            return;
         }
+        cur.push_back(nums[idx]);
+        dfs(idx + 1, nums);
+        cur.pop_back();
+        dfs(idx + 1, nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        dfs(0, nums);
         return res;
     }
 };
