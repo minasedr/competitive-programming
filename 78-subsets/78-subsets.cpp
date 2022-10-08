676236f1955725ca96c665509e2b08502e9bfbd3
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-08 14:33:31 +0300

    Time: 3 ms (76.64%), Space: 7.2 MB (57.53%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        int n = nums.size();
-        
-        for (int i = 0; i < (1 << n); i++) {
-            vector<int> cur;
-            for (int j = 0; j < 32; j++) {
-                if (i & (1 << j))
-                    cur.push_back(nums[j]);
-            }
+    vector<vector<int>> res;
+    vector<int> cur;
+    void dfs(int idx, vector<int>& nums) {
+        if (idx >= nums.size()) {
             res.push_back(cur);
+            return;
         }
+        cur.push_back(nums[idx]);
+        dfs(idx + 1, nums);
+        cur.pop_back();
+        dfs(idx + 1, nums);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        dfs(0, nums);
         return res;
     }
 };
