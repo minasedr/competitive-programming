0147c8145ea72b67d07cadfb5ecfa037de94ced4
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-09 15:36:54 +0300

    Time: 101 ms (34.95%), Space: 8.7 MB (92.72%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n = nums.size(), maxIdx = 0;
-        vector<int> dp(n), par(n, -1), res;
-        
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i ; j++) {
+        int n = nums.size();
+        vector<int> dp(n, 1), par(n, -1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
                 if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
                     dp[i] = dp[j] + 1;
                     par[i] = j;
                 }
             }
-            maxIdx = (dp[i] > dp[maxIdx] ? i : maxIdx);
         }
-        while (maxIdx >= 0) {
-            res.push_back(nums[maxIdx]);
-            maxIdx = par[maxIdx];
+        int idx = max_element(dp.begin(), dp.end()) - dp.begin();
+        
+        vector<int> ans;
+        while (idx != -1) {
+            ans.push_back(nums[idx]);
+            idx = par[idx];
         }
-        return res;
+        return ans;
+        
     }
 };
