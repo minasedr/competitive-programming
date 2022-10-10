e8fd36aee9ce0960a9f9eca98a2148d47375b285
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-10 09:31:35 +0300

    Time: 0 ms (100.00%), Space: 6.3 MB (5.00%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    bool ispal(string s) {
-        string t = s;
-        reverse(t.begin(), t.end());
-        return s == t;
-    }
     string breakPalindrome(string palindrome) {
-        if (palindrome.size() == 1)
-            return "";
-        bool ok = true;
-        for (auto &n : palindrome) {
-            if (n > 'a') {
-                char c = n;
-                n = 'a';
-                if (!ispal(palindrome)) {
-                    ok = false;
-                    break;
-                }
-                n = c;
+        int n = palindrome.size();
+        
+        if (n == 1) return "";
+        bool ok = false;
+        for (int i = 0; i < n / 2; i++) {
+            if (palindrome[i] != 'a') {
+                palindrome[i] = 'a';
+                ok = true;
+                break;
             }
         }
-        if (ok)
+        
+        if (!ok)
             palindrome.back() = 'b';
         return palindrome;
     }
