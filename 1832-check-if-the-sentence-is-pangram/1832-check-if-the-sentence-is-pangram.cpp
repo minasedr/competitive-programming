ac54d1b7cc479a3a54ccc3a7c0c01cf208cc7420
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-17 08:50:05 +0300

    Time: 9 ms (21.04%), Space: 6.7 MB (6.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        vector<int> vis(26);
-        for (char s: sentence)
-            vis[s - 'a']++;
-        for (auto x: vis)
-            if (x == 0) return false;
-        return true;
+        set<char>vis(sentence.begin(), sentence.end());
+        return vis.size() == 26;
     }
 };
