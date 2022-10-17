9b46dae53c1c4d7718326ccd5a47073f91eb182b
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-17 11:55:30 +0300

    Time: 668 ms (58.72%), Space: 160.8 MB (14.34%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        int n = ingredients.size();
-        map<string, set<int>> adj;
-        vector<int> indeg(n);
-        set<string> cur;
-        vector<string> res;
-        
+        int n = recipes.size();
+        unordered_map<string, vector<string>> adj;
+        unordered_map<string, int> mp;
+        vector<int>deg(n);
         for (int i = 0; i < n; i++) {
-            for (auto u: ingredients[i]) {
-                adj[u].insert(i);
-                indeg[i]++;
+            mp[recipes[i]] = i;
+            for (auto gd: ingredients[i]) {
+                adj[gd].push_back(recipes[i]);
+                deg[i]++;
             }
         }
-        
         queue<string> q;
-        
+        for (int i = 0; i < n; i++)
+            if (deg[i] == 0)
+                q.push(recipes[i]);
         for (auto x: supplies)
             q.push(x);
-        
+        set<string> ans;
         while (!q.empty()) {
-            string u = q.front();
+            string node = q.front();
+            ans.insert(node);
             q.pop();
-            cur.insert(u);
-            for (auto v: adj[u]) {
-                if (--indeg[v] == 0)
-                    q.push(recipes[v]);
+            for (auto u: adj[node]) {
+                if (--deg[mp[u]] == 0)
+                    q.push(u);
             }
         }
-        
-        for (auto x: recipes) {
-            if (cur.count(x))
+        vector<string> res;
+        for (auto x: recipes)
+            if (ans.count(x))
                 res.push_back(x);
-        }
         return res;
     }
 };
