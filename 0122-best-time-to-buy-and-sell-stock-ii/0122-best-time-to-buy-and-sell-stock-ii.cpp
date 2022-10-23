5c4fbe821549ffc18be6903532c2fb77e1e965eb
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-23 10:20:35 +0300

    Time: 20 ms (27.68%), Space: 15.2 MB (25.18%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2));
+        dp[0][0] = -prices[0];
+        
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][1] - prices[i], dp[i - 1][0]);
+            dp[i][1] = max(dp[i - 1][0] + prices[i], dp[i - 1][1]);
+        }
+        return max(dp[n - 1][0], dp[n - 1][1]);
+    }
+};
