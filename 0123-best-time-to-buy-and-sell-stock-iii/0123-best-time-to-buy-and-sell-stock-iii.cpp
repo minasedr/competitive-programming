c26215d7e13094f0ed8a45f39214ed791be62eae
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-10-23 22:46:29 +0300

    Time: 1109 ms (31.02%), Space: 171.2 MB (41.07%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp1(n, vector<int>(2, INT_MIN)); // has a stock
-        vector<vector<int>> dp2(n, vector<int>(2, INT_MIN)); // has no stock
+        vector<vector<int>> hasStock(n, vector<int>(2, INT_MIN));
+        vector<vector<int>> noStock(n, vector<int>(2, INT_MIN));
         
-        dp1[0][0] = -prices[0];
+        hasStock[0][0] = -prices[0];
         for (int i = 1; i < n; i++) {
-            dp1[i][0] = max(dp1[i - 1][0], -prices[i]);
-            dp2[i][0] = max(dp2[i - 1][0], dp1[i][0] + prices[i]);
-            dp1[i][1] = max(dp1[i - 1][1], dp2[i][0] - prices[i]);
-            dp2[i][1] = max(dp2[i - 1][1], dp1[i][1] + prices[i]);
+            hasStock[i][0] = max(hasStock[i - 1][0], -prices[i]);
+            noStock[i][0] = max(noStock[i - 1][0], hasStock[i][0] + prices[i]);
+            hasStock[i][1] = max(hasStock[i - 1][1], noStock[i][0] - prices[i]);
+            noStock[i][1] = max(noStock[i - 1][1], hasStock[i][1] + prices[i]);
         }
-        return max(0, dp2[n - 1][1]);
+        return max(0, noStock[n - 1][1]);
     }
 };
