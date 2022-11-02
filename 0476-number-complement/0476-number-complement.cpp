576127ae211c3c38cd67277a3d6da14bc0617b56
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-11-02 10:40:04 +0300

    Time: 3 ms (40.56%), Space: 5.9 MB (39.47%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int findComplement(int num) {
         unsigned ans = 0;
-        int n = log2(num);
-        for (int i = 0; i <= n; i++)
+        
+        for (int i = 0; i <= (int)log2(num); i++)
             if (not (num & (1 << i)))
                 ans += (1 << i);
         
