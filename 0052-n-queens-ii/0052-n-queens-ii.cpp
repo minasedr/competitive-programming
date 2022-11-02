bc0b5abc3c0572473f8b1b9e202e4bb18e22e648
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-11-02 14:37:44 +0300

    Time: 33 ms (21.08%), Space: 7.6 MB (22.42%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int totalNQueens(int n) {
-        set<array<int,2>> vis;
+        unordered_set<int> diag, anti_diag, vert;
 
         function<bool(int, int)> inbound = [&](int r, int c) {
             return (r >= 0 and r < n and c >= 0 and c < n);
@@ -9,33 +9,33 @@ public:
         function<bool(int, int)> valid = [&](int r, int c) {
             if (not inbound(r, c))
                 return false;
-            for (auto [nr, nc] : vis) {
-                if (nr + nc == r + c)
-                    return false;
-                if (nr - nc == r - c)
-                    return false;
-                if (nr == r || nc == c)
-                    return false;
-            }
+            if (anti_diag.count(r - c) || diag.count(r + c))
+                return false;
+            if (vert.count(c))
+                return false;
             return true;
         };
         
         function<int(int, int)> dfs;
         dfs = [&](int r, int c) {
-            if (r >= n - 1) return 1;
+            if (r >= n - 1)
+                return 1;
             int ans = 0;
-            
-            vis.insert({r, c});
+            diag.insert(r + c);
+            anti_diag.insert(r - c);
+            vert.insert(c);
             for (int j = 0; j < n; j++) {
                 if (valid(r + 1, j))
                     ans += dfs(r + 1, j);
             }
-            vis.erase({r, c});
+            diag.erase(r + c);
+            anti_diag.erase(r - c);
+            vert.erase(c);
             return ans;
         };
         int cnt = 0;
         for (int j = 0; j < n; j++)
             cnt += dfs(0, j);
-        return cnt;
+        return cnt;    
     }
 };
