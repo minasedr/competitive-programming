ea62f00e6882a291861703dfcbc5d509f190fa60
Minase Driba <93864556+minasedr@users.noreply.github.com>
2022-11-10 09:52:06 +0300

    Time: 79 ms (58.02%), Space: 22.4 MB (41.73%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        unordered_set<int> st;
+        int N = nums.size();
+        vector<int>cnt(N + 1);
         int miss = 0, sum = 0, m = nums.size();
+
         for (auto n: nums) {
-            if (st.count(n)) {
+            if (cnt[n]) {
                 miss = n;
                 continue;
             }
-            st.insert(n);
+            cnt[n]++;
             sum += n;
         }
         return {miss, (m + 1) * m / 2 - sum};
