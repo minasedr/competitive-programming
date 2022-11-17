af738aad18b76c375ea8f8034c6531f72e8566f3
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2022-11-18 00:43:26 +0300

    Time: 39 ms (63.79%), Space: 16.5 MB (81.60%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 1e9);
-        dp[0] = 0;
+        int max_reached = 0, jumps = 0, cur = 0;
+        int ans = 0;
+        
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (j + nums[j] >= i)
-                    dp[i] = min(dp[i], dp[j] + 1);
+            ans = jumps;
+            max_reached = max(max_reached, i + nums[i]);
+            if (i == cur) {
+                cur = max_reached;
+                jumps++;
             }
         }
-        return dp[n - 1];
+        return ans;
     }
 };
