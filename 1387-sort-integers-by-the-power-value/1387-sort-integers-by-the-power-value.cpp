296a222e658604ca28a5a0203855d0152f9065d7
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2022-11-20 14:11:23 +0300

    Time: 539 ms (7.57%), Space: 302.2 MB (5.02%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -3,10 +3,10 @@ public:
     int getKth(int lo, int hi, int k) {
         vector<int> dp(1000000, -1);
         priority_queue<array<int, 2>, vector<array<int, 2>>, greater<>> pq; 
-        
+        dp[1] = 0;
+
         function<int(int)> power;
         power = [&](int n) {
-            if (n == 1) return 0;
             if (dp[n] != -1)
                 return dp[n];
             int cnt = 1 + (n & 1 ? power(3 * n + 1) : power(n / 2));
