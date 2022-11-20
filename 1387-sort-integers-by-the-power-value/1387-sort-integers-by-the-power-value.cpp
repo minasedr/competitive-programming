87f35f99d8905e7d29c854e2c6387322a11e92ca
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2022-11-20 14:10:08 +0300

    Time: 678 ms (5.03%), Space: 302.4 MB (5.02%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int power(int n) {
-        if (n == 1)
-            return 0;
-        if (n % 2 == 0)
-            return 1 + power(n / 2);
-        return 1 + power(3 * n + 1);
-    }
     int getKth(int lo, int hi, int k) {
-        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<>> pq;
-        for (int i = lo; i <= hi; i++)
+        vector<int> dp(1000000, -1);
+        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<>> pq; 
+        
+        function<int(int)> power;
+        power = [&](int n) {
+            if (n == 1) return 0;
+            if (dp[n] != -1)
+                return dp[n];
+            int cnt = 1 + (n & 1 ? power(3 * n + 1) : power(n / 2));
+            return dp[n] = cnt;
+        };
+        
+        for(int i = lo; i <= hi; i++)
             pq.push({power(i), i});
         
-        int ans = -1;
-        while(k--) {
-            ans = pq.top()[1];
+        while(--k)
             pq.pop();
-        }
-        return ans;
+        
+        return pq.top()[1];
     }
 };
