71c9f3a84c5c32f944533f56a5c4f1b44e7b23fb
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2022-11-27 01:01:26 +0300

    Time: 4 ms (46.35%), Space: 6.1 MB (99.75%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
-        int n = sequence.size(), m = word.size();
-        int ans = 0;
-        
-        for (int i = 0; i < n; i++) {
-            int idx = i, cur = 0;
-            while (idx < n) {
-                if (sequence.substr(idx, m) == word)
-                    cur++, idx += m;
-                else
-                    break;
-            }
-            ans = max(ans, cur);
+        int cnt = 0;
+        string tmp = word;
+        while (sequence.find(tmp) != string::npos) {
+            cnt++;
+            tmp += word;
         }
-        return ans;
+        return cnt;
     }
 };
