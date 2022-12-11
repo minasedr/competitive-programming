eee8ce241ddcbba65358a7f9cca55ac4fc09f31a
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2022-12-11 23:54:12 +0300

    Time: 3 ms (98.49%), Space: 9.7 MB (77.29%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        vector<int> cur;
         int n = arr.size();
+        int j = n + count(arr.begin(), arr.end(), 0);
         
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                cur.push_back(0);
-                cur.push_back(0);
-            } else
-                cur.push_back(arr[i]);
+        for (int i = n - 1; i >= 0; i--) {
+            if (--j < n)
+                arr[j] = arr[i];
+            if (arr[i] == 0 and --j < n)
+                arr[j] = 0;
         }
-        vector<int> ans(cur.begin(), cur.begin() + n);
-        arr = ans;
     }
 };
