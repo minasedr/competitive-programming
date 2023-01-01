afe3e8d51afa52bf8a92e47f7b9d528e15445a98
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-01 21:56:24 +0300

    Time: 3 ms (40.81%), Space: 6.2 MB (13.63%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1);
         function<int(int)> dfs;
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            for (int j = 1; j <= i; j++)
+                dp[i] += dp[j - 1] * dp[i - j];
+        
+        return dp[n];
+        /*
         dfs = [&](int i) {
             if (i <= 1) return 1;
             if (dp[i] != -1) return dp[i];
@@ -12,5 +19,6 @@ public:
             return dp[i] = cnt;
         };
         return dfs(n);
+        */
     }
 };
