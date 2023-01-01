15002332494c054f2845254aeaceecbe563d1390
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-01 17:49:23 +0300

    Time: 3 ms (35.27%), Space: 5.9 MB (90.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        function<int(int)> dfs;
-        vector<int> dp(45, -1);
-        dfs = [&](int i) {
-            if (i >= n)
-                return (i == n ? 1 : 0);
-            if (dp[i] != -1) return dp[i];
-            return dp[i] = dfs(i + 1) + dfs(i + 2);
-        };
-        return dfs(0);
+        int dp[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
     }
 };
