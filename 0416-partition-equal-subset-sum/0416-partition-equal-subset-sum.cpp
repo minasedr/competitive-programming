f5051dc52f835c0f0a7f216e1069c9af7b9ffa72
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-05 11:13:22 +0300

    Time: 316 ms (60.89%), Space: 9.7 MB (87.20%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,19 +3,16 @@ public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> dp(n + 1, vector<int>(2 * sum + 1, -1));
-        int k = sum;
-        function<bool(int, int)> dfs;
-        dfs = [&](int i, int cur) {
-            if (i >= n)
-                return (cur == 0);
-            if (dp[i][cur + k] != -1)
-                return dp[i][cur + k] > 0;
-            if (dfs(i + 1, cur + nums[i]) or dfs(i + 1, cur - nums[i]))
-                return (dp[i][cur + k] = 1) > 0;
-            return (dp[i][cur + k] = 0) > 0;
-        };
-        
-        return dfs(0, 0);
+        if (sum % 2) return false;
+        sum /= 2;
+        vector<bool> dp(sum + 1);
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int w = sum; w >= 0; w--) {
+                if (w >= nums[i - 1])
+                    dp[w] = (dp[w] | dp[w - nums[i - 1]]);
+            }
+        }
+        return dp[sum];
     }
 };
