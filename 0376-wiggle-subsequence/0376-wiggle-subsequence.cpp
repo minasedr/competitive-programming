15e730c2bdb0a6ffbb60f6f0494b26ea5e174250
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-05 11:59:54 +0300

    Time: 24 ms (17.75%), Space: 7.3 MB (31.81%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<int>>> dp(2, vector<vector<int>>(n + 1, vector<int>(3000, -1)));
+        vector<int> pos(n + 1, 1), neg(n + 1, 1);
         
-        function<int(int, int, bool)> dfs;
-        dfs = [&](int i, int prev, bool diff) {
-            if (i >= n) return 0;
-            if (dp[diff][i][prev + 1] != -1)
-                return dp[diff][i][prev + 1];
-            int cnt = 0;
-            if (nums[i] - prev > 0 and not diff)
-                cnt = 1 + dfs(i + 1, nums[i], !diff);
-            else if (nums[i] - prev < 0 and diff)
-                cnt = 1 + dfs(i + 1, nums[i], !diff);
-            cnt = max(cnt, dfs(i + 1, prev, diff));
-            return dp[diff][i][prev + 1] = cnt;
-        };
-        
-        return max(dfs(0, 1001, true), dfs(0, -1, false));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] < nums[j])
+                    neg[i] = max(neg[i], pos[j] + 1);
+                else if (nums[i] > nums[j])
+                    pos[i] = max(pos[i], neg[j] + 1);
+                else {
+                    neg[i] = max(neg[i], neg[i - 1]);
+                    pos[i] = max(pos[i], pos[i - 1]);
+                }
+            }
+        }
+        return max(pos[n - 1], neg[n - 1]);
     }
 };
