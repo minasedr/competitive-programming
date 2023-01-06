b1df65b18117f157c05e1e1a2cce3f2cf741e971
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-06 14:55:57 +0300

    Time: 2 ms (49.55%), Space: 6.1 MB (15.39%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
         if (n == 1) return 10;
-        vector<int> dp(11, -1);
         function<int(int)> f;
-        
         f = [&](int k) {
             int ans = 9;
             for (int i = 0, q = 9; i < k - 1 and q > 0; i++, q--)
