2c95d2efd8cc7f32fc64447cc224b9314c6b1c28
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-08 15:05:33 +0300

    Time: 3 ms (86.72%), Space: 10.1 MB (18.13%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n = days.size();
-        int cnt[]{1,7, 30};
-        vector<int> dp(n + 1, -1);
+        int n = days.back();
+        set<int> vis(days.begin(), days.end());
+        vector<int> dp(n + 1);
         
-        function<int(int)> dfs;
-        dfs = [&](int i) {
-            if (i >= n) return 0;
-            if (dp[i] != -1) 
-                return dp[i];
-            int cost = 1e6;
-            for (int j = 0; j < 3; j++) {
-                int nxt = lower_bound(days.begin() + i, days.end(), days[i] + cnt[j]) - days.begin();
-                for (int k = i + 1; k <= nxt; k++)
-                    cost = min(cost, costs[j] + dfs(k));
-            }
-            return dp[i] = cost;
-        };
-        
-        return dfs(0);
+        for (int i = 1; i <= n; i++) {
+            if (vis.count(i))
+                dp[i] = min({
+                    dp[max(0, i - 1)] + costs[0],
+                    dp[max(0, i - 7)] + costs[1],
+                    dp[max(0, i - 30)] + costs[2],
+                });
+            else
+                dp[i] = dp[i - 1];
+        }
+        return dp[n];
     }
 };
