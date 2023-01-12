206d0e58fd144fb8c9d9832b4e37c36ec339ddeb
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-13 00:32:06 +0300

    Time: 155 ms (86.25%), Space: 65.4 MB (68.45%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int maxJump(vector<int>& stones) {
-        int n = stones.size(), ans = 0;
-        vector<bool> vis(n + 1);
+        int ans = 0;
+        if (stones.size() == 2)
+            return stones[1];
         
-        for (int i = 2; i < n; i += 2) {
+        for (int i = 2; i < stones.size(); i++)
             ans = max(ans, stones[i] - stones[i - 2]);
-            vis[i] = vis[i - 2] = true;
-        }
         
-        int last = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (vis[i]) continue;
-            ans = max(ans, stones[last] - stones[i]);
-            last = i;
-        }
         return ans;
     }
 };
