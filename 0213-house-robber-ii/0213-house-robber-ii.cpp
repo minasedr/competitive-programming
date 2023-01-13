c0c2537e34b78cf88658f882ae793655a09bba7d
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-13 16:25:33 +0300

    Time: 5 ms (26.40%), Space: 7.9 MB (45.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return nums[0];
-        vector<int> dp(n + 1, -1);
-        
-        function<int(int, int)> dfs;
-        dfs = [&](int i, int n) {
-            if (i >= n) return 0;
-            if (dp[i] != -1)
-                return dp[i];
-            int rob = nums[i] + dfs(i + 2, n);
-            int dont = dfs(i + 1, n);
-            return dp[i] = max(rob, dont);;
-        };
-        int ans = dfs(0, n - 1);
-        fill(dp.begin(), dp.end(), -1);
-        ans = max(ans, dfs(1, n));
+        if (n == 1)
+            return nums[0];
+        vector<int> dp(n + 1);
+        dp[1] = nums[1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        int ans = *max_element(dp.begin(), dp.end());
+        fill(dp.begin(), dp.end(), 0);
+        dp[0] = nums[0];
+        dp[1] = max(dp[0], nums[1]);
+        for (int i = 2; i < n - 1; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        ans = max(ans, *max_element(dp.begin(), dp.end()));
         return ans;
     }
 };
