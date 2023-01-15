2d3d74266c33a8558472ccff17b03f582b3be820
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-15 12:49:15 +0300

    Time: 281 ms (78.05%), Space: 9.7 MB (88.91%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len = strs.size();
-        vector<vector<vector<int>>> dp(len + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, -1)));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         
-        function<int(int, int, int)> dfs;
-        dfs = [&](int i, int x, int y) {
-            if (i >= len) return 0;
-            if (dp[i][x][y] != -1)
-                return dp[i][x][y];
+        for (int i = 0; i < len; i++) {
             int one = count(strs[i].begin(), strs[i].end(), '1');
-            int zero  = strs[i].size() - one;
-            int pick = 0, dont = 0;
-            if (x + zero <= m and y + one <= n)
-                pick = 1 + dfs(i + 1, x + zero, y + one);
-            dont = dfs(i + 1, x, y);
-            return dp[i][x][y] = max(pick, dont);
-        };
-        
-        return dfs(0, 0, 0);
+            int zero = strs[i].size() - one;
+            for (int j = m; j >= zero; j--) {
+                for (int k = n; k >= one; k--) {
+                    dp[j][k] = max(dp[j][k], 1 + dp[j - zero][k - one]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
