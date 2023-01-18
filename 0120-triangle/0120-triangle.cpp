b66d40f9d297b3fd8103ca837a39a3feb269ff8d
Minase Gurmu <93864556+minasedr@users.noreply.github.com>
2023-01-18 16:25:31 +0300

    Time: 3 ms (97.97%), Space: 9 MB (35.02%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size(), m = triangle[n - 1].size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 1e6));
+        dp[0][0] = triangle[0][0];
         
-        function<int(int, int)> dfs;
-        dfs = [&](int r, int c) {
-            if (r >= n) return 0;
-            if (c >= triangle[r].size())
-                return (int)1e6;
-            if (dp[r][c] != -1)
-                return dp[r][c];
-            return dp[r][c] = triangle[r][c] + min(dfs(r + 1, c), dfs(r + 1, c + 1));
-        };
-        
-        return dfs(0, 0);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                if (j == 0)
+                    dp[i][j] = dp[i - 1][j];
+                int topL = 1e6;
+                if (j > 0)
+                    topL = dp[i - 1][j - 1];
+                int top = 1e6;
+                if (j < triangle[i].size() - 1)
+                    top = dp[i - 1][j];
+                dp[i][j] = triangle[i][j] + min(top, topL);
+            }
+        }
+        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
     }
 };
