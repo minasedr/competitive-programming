193fa9b96bd2cc79fe13f496075e9f0f8f853eb0
Minase Driba <93864556+minasedr@users.noreply.github.com>
2023-02-02 18:31:07 +0300

    Time: 7 ms (23.56%), Space: 8.4 MB (62.09%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        set<int> vis;
+        unordered_set<int> vis;
         for (int n: nums)
             if (n) vis.insert(n);
         
