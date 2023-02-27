4107e079e2a4128ad7ed41b1a269482aafb56b05
Minase Driba <93864556+minasedr@users.noreply.github.com>
2023-02-27 18:45:44 +0300

    Time: 0 ms (100.00%), Space: 8.7 MB (5.98%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        int mx = 0, year = 0;
+        vector<int>pref(2055);
         
+        for (auto x: logs) {
+            pref[x[0]]++;
+            pref[x[1]]--;
+        }
+        for (int i = 1; i < 2055; i++)
+            pref[i] += pref[i - 1];
+        
+        int year = 0, cnt = 0;
         for (int i = 1950; i <= 2050; i++) {
-            int alive = 0;
-            for (auto x: logs)
-                alive += (x[0] <= i and i < x[1]);
-            if (mx < alive) {
-                mx = alive;
+            if (pref[i] > cnt) {
+                cnt = pref[i];
                 year = i;
             }
         }
-        
         return year;
     }
 };
