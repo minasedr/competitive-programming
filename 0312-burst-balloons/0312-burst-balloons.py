f9fcd4a4309595bc0c91c70d948feb0d86580ad1
Minase Driba <93864556+minasedr@users.noreply.github.com>
2023-03-02 19:17:12 +0300

    Time: 13337 ms (7.60%), Space: 30.8 MB (24.85%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -3,14 +3,18 @@ class Solution:
         nums.insert(0, 1)
         nums.append(1)
         
-        @cache
+        memo = {}
         def dfs(left, right):
             if left > right:
                 return 0
+            
+            if (left, right) in memo:
+                return memo[(left, right)]
             ans = 0
             for k in range(left, right + 1):
                 ans = max(ans, dfs(left, k - 1) + (nums[left - 1] * nums[k] * nums[right + 1]) + dfs(k + 1, right))
             
+            memo[(left, right)] = ans
             return ans
         
         return dfs(1, len(nums) - 2)
