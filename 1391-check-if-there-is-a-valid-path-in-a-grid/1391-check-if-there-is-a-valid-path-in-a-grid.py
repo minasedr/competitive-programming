efc3b66b60b225d54068c68b0297fe03194ba756
Minase Driba <93864556+minasedr@users.noreply.github.com>
2023-04-07 13:39:02 +0300

    Time: 1454 ms (80.13%), Space: 117.7 MB (34.62%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,6 +1,8 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
-        destination = (len(grid)-1, len(grid[0]) - 1)
+        ROW = len(grid)
+        COL = len(grid[0])
+        visited = [[False] * COL for _ in range(ROW)]
         
         directions = {1: [(0,-1),(0,1)], 
                       2: [(-1,0),(1,0)], 
@@ -9,19 +11,16 @@ class Solution:
                       5: [(0,-1),(-1,0)], 
                       6: [(0,1),(-1,0)]}
 		
-        inbound = lambda row, col: 0 <= row < len(grid) and 0 <= col < len(grid[0])
-		
-        visited = set([(0, 0)])
-
+        inbound = lambda row, col: 0 <= row < ROW and 0 <= col < COL
+        
         def dfs(row, col):
-            if (row, col) == destination:
+            if (row, col) == (ROW - 1, COL - 1):
                 return True
-            visited.add((row, col))
+            visited[row][col] = True
             for dr, dc in directions[grid[row][col]]:
                 nr = row + dr
                 nc = col + dc
-                
-                if not inbound(nr, nc) or (nr, nc) in visited:
+                if not inbound(nr, nc) or visited[nr][nc]:
                     continue
                 if (-dr, -dc) in directions[grid[nr][nc]]:
                     if dfs(nr, nc):
